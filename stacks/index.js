315cde4c5a709efde0a9a42a59f84554d361d31a
e <41101885+rde9@users.noreply.github.com>
2022-06-17 15:54:07 +0800

    Add DB & S3 Bucket

---
+++ stacks/index.js
@@ -0,0 +1,12 @@
+import { StorageStack } from "./StorageStack";
+
+export default function main(app) {
+  app.setDefaultFunctionProps({
+    runtime: "nodejs16.x",
+    srcPath: "backend",
+    bundle: {
+      format: "esm",
+    },
+  });
+  app.stack(StorageStack);
+}
